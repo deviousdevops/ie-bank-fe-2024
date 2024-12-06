7ff2446d7b2965c66ee194b1a330c91e4d6aa8a5
Andres <ajtaboada04@gmail.com>
2024-12-06 22:15:40 +0100

    temporarily remove app insights

--- src/appInsights.js
+++ src/appInsights.js
@@ -1,27 +1,27 @@
-import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+// import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
-let appInsights = null;
+// let appInsights = null;
 
-export const initializeAppInsights = () => {
-    appInsights = new ApplicationInsights({
-        config: {
-            connectionString: process.env.VUE_APP_APP_INSIGHTS_CONNECTION_STRING,
-            enableAutoRouteTracking: true,
-            disableFetchTracking: false,
-            disableAjaxTracking: false
-        }
-    });
-    appInsights.loadAppInsights();
-};
+// export const initializeAppInsights = () => {
+//     appInsights = new ApplicationInsights({
+//         config: {
+//             connectionString: process.env.VUE_APP_APP_INSIGHTS_CONNECTION_STRING,
+//             enableAutoRouteTracking: true,
+//             disableFetchTracking: false,
+//             disableAjaxTracking: false
+//         }
+//     });
+//     appInsights.loadAppInsights();
+// };
 
-export const trackEvent = (name, properties = {}) => {
-    if (appInsights) {
-        appInsights.trackEvent({ name }, properties);
-    }
-};
+// export const trackEvent = (name, properties = {}) => {
+//     if (appInsights) {
+//         appInsights.trackEvent({ name }, properties);
+//     }
+// };
 
-export const trackError = (error) => {
-    if (appInsights) {
-        appInsights.trackException({ exception: error });
-    }
-};
+// export const trackError = (error) => {
+//     if (appInsights) {
+//         appInsights.trackException({ exception: error });
+//     }
+// };
